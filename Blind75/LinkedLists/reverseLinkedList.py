25b820a75c7ed922436602d5ed6d6056b013cf98
jodiezhuu <jodiezhu12@gmail.com>
2022-09-26 23:58:17 -0400

    reverse linked list algo

---
+++ Blind75/LinkedLists/reverseLinkedList.py
@@ -0,0 +1,11 @@
+# Given the head of a singly linked list, reverse the list, and return the reversed list.
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        previous = None
+        curr = head
+        while curr:
+            next_node = curr.next
+            curr.next = previous
+            previous = curr
+            curr = next_node
+        return previous
